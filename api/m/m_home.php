858d7478310547b2a2ae5c5b1874d635305a0e58
ISSOUFOU MAMANE Djibril <idjibril@mephi.fr>
2019-08-11 14:25:32 -0700

    #

--- api/m/m_home.php
+++ api/m/m_home.php
@@ -10,7 +10,7 @@
 		} else {
 			return 1;
 		}
-		$req = $base->query("SELECT DISTINCT cf.id, cl.idClient, c.idCollecteur, c.nomCollecteur, c.prenomCollecteur, c.tel1Collecteur, c.emailCollecteur, c.idCommune, cr.dateRecuperation, cr.dateDebutCollecte, cr.montantAverser, cr.frequence, cr.statutClient, cr.typeFrequence, cf.dateVersement, cf.dateProchainVersement, cf.montantVerse, cf.commentaire, cf.type, cf.statut FROM main_client cl INNER JOIN `collecteur_recuperer_client` cr ON cr.idClient=cl.idClient INNER JOIN main_collecteur c ON c.idCollecteur=cr.idCollecteur LEFT JOIN client_faireversement_collecteur cf ON cl.idClient=cf.idClient WHERE (cr.statutCLient='pasOk' OR cr.statutCLient='pas') AND cr.idClient='$idClient' ORDER BY cr.id DESC");
+		$req = $base->query("SELECT DISTINCT cf.id, cl.idClient, c.idCollecteur, c.nomCollecteur, c.prenomCollecteur, c.tel1Collecteur, c.emailCollecteur, c.idCommune, cr.dateRecuperation, cr.dateDebutCollecte, cr.montantAverser, cr.frequence, cr.statutClient, cr.typeFrequence, cf.dateVersement, cf.dateProchainVersement, cf.montantVerse, cf.commentaire, cf.type, cf.statut FROM main_client cl INNER JOIN `collecteur_recuperer_client` cr ON cr.idClient=cl.idClient INNER JOIN main_collecteur c ON c.idCollecteur=cr.idCollecteur LEFT JOIN client_faireversement_collecteur cf ON cl.idClient=cf.idClient WHERE (cr.statutCLient='pasOk' OR cr.statutCLient='pas') AND cr.idClient='$idClient' ORDER BY cr.id, cf.dateVersement DESC");
 		if ($res = $req->fetchAll()) {
 			$out[1] = $res;
 		} else { 
@@ -28,7 +28,7 @@
 		} else {
 			return 1;
 		}
-		$req = $base->query("SELECT DISTINCT cf.id, co.idCollecteur, c.idClient, c.nomClient, c.prenomClient, c.tel1Client, c.emailClient, c.idCommune, cr.dateRecuperation, cr.dateDebutCollecte, cr.montantAverser, cr.frequence, cr.statutClient, cr.typeFrequence, cf.dateVersement, cf.dateProchainVersement, cf.montantVerse, cf.commentaire, cf.type, cf.statut FROM main_collecteur co INNER JOIN `collecteur_recuperer_client` cr ON cr.idCollecteur=co.idCollecteur INNER JOIN main_client c ON c.idClient=cr.idClient LEFT JOIN client_faireversement_collecteur cf ON c.idClient=cf.idClient WHERE (cr.statutCLient='pasOk' OR cr.statutCLient='pas') AND cr.idCollecteur='$idCollecteur' ORDER BY cr.id DESC");
+		$req = $base->query("SELECT DISTINCT cf.id, co.idCollecteur, c.idClient, c.nomClient, c.prenomClient, c.tel1Client, c.emailClient, c.idCommune, cr.dateRecuperation, cr.dateDebutCollecte, cr.montantAverser, cr.frequence, cr.statutClient, cr.typeFrequence, cf.dateVersement, cf.dateProchainVersement, cf.montantVerse, cf.commentaire, cf.type, cf.statut FROM main_collecteur co INNER JOIN `collecteur_recuperer_client` cr ON cr.idCollecteur=co.idCollecteur INNER JOIN main_client c ON c.idClient=cr.idClient LEFT JOIN client_faireversement_collecteur cf ON c.idClient=cf.idClient WHERE (cr.statutCLient='pasOk' OR cr.statutCLient='pas') AND cr.idCollecteur='$idCollecteur' ORDER BY cr.id, cf.dateVersement DESC");
 		if ($res = $req->fetchAll()) {
 			$out[1] = $res;
 		} else { 
--- api/m/m_signup.php
+++ api/m/m_signup.php
@@ -7,12 +7,20 @@
 		global $base;
 		$req = $base->query("SELECT * FROM main_client WHERE tel1Client = '$tel1Client'");
 		if ($res = $req->fetch()) {
+
+			$idUser = $res['idClient'];
+			$base->exec("INSERT INTO `log` (`libelle`, `details`, `typeUser`, `idUser`) VALUES ('inscription', 'Echec Inscription', 'CLIENT', '$idUser')");
 			return 1;
+
 		} else {
+
 			$sql = "INSERT INTO `main_client` (`idClient`, `nomClient`, `prenomClient`, `tel1Client`, `emailClient`, `motDePasseClient`, `idCommune`) VALUES (NULL, '$nomClient', '$prenomClient', '$tel1Client', '$emailClient', '$motDePasseClient', $idCommune)";
 			$base->exec($sql);
 			$req = $base->query("SELECT * FROM main_client WHERE tel1Client = '$tel1Client'");
 			if ($res = $req->fetch()) {
+
+				$idUser = $res['idClient'];
+				$base->exec("INSERT INTO `log` (`libelle`, `details`, `typeUser`, `idUser`) VALUES ('inscription', 'Inscription reussie', 'CLIENT', '$idUser')");
 				return $res;
 			} else {
 				return 1;
@@ -25,12 +33,17 @@
 		global $base;
 		$req = $base->query("SELECT * FROM main_collecteur WHERE tel1Collecteur = '$tel1Collecteur'");
 		if ($res = $req->fetch()) {
+			$idUser = $res['idCollecteur'];
+			$base->exec("INSERT INTO `log` (`libelle`, `details`, `typeUser`, `idUser`) VALUES ('inscription', 'Echec Inscription', 'COLLECTEUR', '$idUser')");
 			return 1;
 		} else {
 			$sql = "INSERT INTO `main_collecteur` (`idCollecteur`, `nomCollecteur`, `prenomCollecteur`, `tel1Collecteur`, `emailCollecteur`, `motDePasseCollecteur`, `idCommune`) VALUES (NULL, '$nomCollecteur', '$prenomCollecteur', '$tel1Collecteur', '$emailCollecteur', '$motDePasseCollecteur', $idCommune)";
 			$base->exec($sql);
 			$req = $base->query("SELECT * FROM main_collecteur WHERE tel1Collecteur = '$tel1Collecteur'");
 			if ($res = $req->fetch()) {
+
+				$idUser = $res['idCollecteur'];
+				$base->exec("INSERT INTO `log` (`libelle`, `details`, `typeUser`, `idUser`) VALUES ('inscription', 'Inscription reussie', 'COLLECTEUR', '$idUser')");
 				return $res;
 			} else {
 				return 1;
